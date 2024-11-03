90f0b4f7a8a25af2788746483752fc2e4a6aec4a
Alex Davies <alex@frogtrade.com>
2024-11-03 01:20:26 +0000

    Optimisations + Fixed issue with webcam performance

--- person-blocker.py
+++ person-blocker.py
@@ -1,34 +1,44 @@
+import time
 import cv2
 import numpy as np
 import logging
 from ultralytics import YOLO
+import torch
 
-# get prediction result
-# get vid cap device
-USE_REMOTE = False
-if not USE_REMOTE:
-    cap = cv2.VideoCapture(0)
-else:
-    cap = cv2.VideoCapture("http://10.253.169.237:8080/video") 
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+DOWNSCALE_HEIGHT = 1000
+ENABLE_GPU = True
+
+try: # GPU Support?
+    torch.cuda.set_device(0)
+    torch.cuda.is_available = lambda : ENABLE_GPU
+except:
+    pass
 
 seg_model = YOLO("yolo11n-seg.pt")
 
-threshold = 0.55
-TARGET_HEIGHT = 1000
-
-DILATE_RADIUS = 20
-DILATE_ELEMENT = (DILATE_RADIUS * 2 + 1, DILATE_RADIUS * 2 + 1)  # Make kernel size (2*size + 1)
-DILATE_KERNEL = cv2.getStructuringElement(cv2.MORPH_RECT, DILATE_ELEMENT)
-
 logging.getLogger('ultralytics').setLevel(logging.ERROR)
+
+# get vid cap device
+USE_REMOTE = False
+if not USE_REMOTE:
+    cap = cv2.VideoCapture(0, cv2.CAP_MSMF, params=[cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY])
+else:
+    cap = cv2.VideoCapture("http://10.253.169.237:8080/video", cv2.CAP_MSMF, params=[cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY])
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+
+frames = 0
+last_time = time.perf_counter()
 
 # loop through frame
 while cap.isOpened():
     ret, frame = cap.read()
+
     frame_size = (frame.shape[1], frame.shape[0])
 
-    downscale_factor =  TARGET_HEIGHT / frame_size[0]
+    downscale_factor =  DOWNSCALE_HEIGHT / frame_size[0]
 
     downscale_size = (int(frame_size[0] * downscale_factor), int(frame_size[1] * downscale_factor))
     downscaled = cv2.resize(frame, downscale_size, interpolation=cv2.INTER_CUBIC)
@@ -38,47 +48,53 @@ while cap.isOpened():
 
     merged_mask = np.ones((downscale_size[1], downscale_size[0]), dtype=np.uint8)
     
-    outlines = np.zeros((frame_size[1], frame_size[0], 1), dtype=np.uint8)
+    outlines = np.full((frame_size[1], frame_size[0]), 0, dtype=np.uint8)
 
     for result in results:
+        if result.masks is None:
+            break
         for mask, box in zip(result.masks, result.boxes):
             name = seg_model.names[int(box.cls)]
             if name != 'person':
                 continue
             points = np.int32([mask.xy])
             cv2.fillPoly(merged_mask, points, 0)
-            cv2.polylines(outlines, (points / downscale_factor).astype(np.int32), True, 255, 1)
+            cv2.polylines(outlines, np.rint(points / downscale_factor).astype(np.int32), True, 200, 2)
 
     scaled_mask = cv2.resize(merged_mask, frame_size)
-    
     final = cv2.bitwise_and(frame, frame, mask=scaled_mask)
 
-    final = final.astype(np.int16)
-
-    lowres_size = (frame_size[0] // 15, frame_size[1] // 15)
-    lowres_effect = (255 - cv2.cvtColor(cv2.resize(frame, lowres_size, interpolation=cv2.INTER_NEAREST), cv2.COLOR_BGR2GRAY)).astype(np.int16)
-    lowres_noise = np.random.randint(-15, 15, (lowres_size[1], lowres_size[0]), dtype=np.int16)
-    lowres_effect += 60 + lowres_noise
-    lowres_effect = np.clip(lowres_effect, 120, 255).astype(np.uint8)
-    effect = cv2.blur(cv2.resize(lowres_effect, frame_size, interpolation=cv2.INTER_NEAREST), (5, 5))
-    noise = np.random.randint(0, 25, (frame_size[1] // 2, frame_size[0] // 2), dtype=np.int16)
-    effect = np.clip(effect.astype(np.int16) + cv2.resize(noise, frame_size, interpolation=cv2.INTER_CUBIC), 0, 255).astype(np.uint8)
+    lowres_size = (frame_size[0] // 20, frame_size[1] // 20)
+    lowres_effect = (255 - cv2.cvtColor(cv2.resize(frame, lowres_size, interpolation=cv2.INTER_LINEAR), cv2.COLOR_BGR2GRAY))
+    lowres_noise = np.random.randint(20, 50, (lowres_size[1], lowres_size[0]), dtype=np.uint8)
+    lowres_effect += lowres_noise
+    lowres_effect[lowres_effect < lowres_noise] = 255 # Underflow Protection
+    lowres_effect[lowres_effect < 100] = 100 # Clamp low values so we don't get too dark colours
+    effect = cv2.resize(lowres_effect.astype(np.uint8), frame_size, interpolation=cv2.INTER_NEAREST)
+    noise = np.random.randint(0, 75, (frame_size[1] // 2, frame_size[0] // 2), dtype=np.uint8)
+    noise = cv2.resize(noise, frame_size, interpolation=cv2.INTER_LINEAR)
+    effect += noise
+    effect[effect < noise] = 255
+    
     masked_effect = cv2.bitwise_and(effect, effect, mask=(1 - scaled_mask))
-
-    outlines = cv2.cvtColor(outlines, cv2.COLOR_GRAY2BGR)
-    outlines = cv2.blur(outlines, (3, 3))
-
-    final = np.clip(final - outlines, 0, 255)
+    masked_effect -= outlines
+    masked_effect[masked_effect > (255 - outlines)] = 0
+    masked_effect = cv2.blur(masked_effect, (3, 3))
 
     final += cv2.cvtColor(masked_effect, cv2.COLOR_GRAY2BGR)
-
-    final = np.clip(final, 0, 255).astype(np.uint8)
+    
+    frames += 1
+    curr_time = time.perf_counter()
+    if curr_time - last_time > 1:
+        print("FPS:", frames)
+        last_time = curr_time
+        frames = 0
 
     # Show result to user on desktop
     cv2.imshow('Output', final)
-    
+
     # Break loop outcome 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release() # Releases webcam or capture device
